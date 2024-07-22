9462445a9360fb3bdd75f90e16410402f19bacd1
Fabian2023 <fasalpe34@gmail.com>
2024-07-22 09:22:43 -0500

    feat: :art: BackgroundLanding

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,21 +1,18 @@
 
 import { useNavigate } from 'react-router-dom';
-
+import background from "../images/background.png";
 
 const WelcomePage = () => {
   const navigate = useNavigate();
 
- 
-
-  const handlerClic =()=>{
-    navigate("/Photo")
+  const handlerClic = () => {
+    navigate("/Photo");
   }
 
-
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
       <div className="text-center">
-        <h1 className="text-2xl font-bold mb-4">Welcome Take a Pic</h1>
+        <h1 className="text-2xl font-bold mb-4 text-white">Welcome Take a Pic</h1>
         <button onClick={handlerClic} className="px-4 py-2 bg-blue-500 text-white rounded">Next</button>
       </div>
     </div>
