09853ff69c0a762c59881763c202d56155abc193
Luis Arturo Hernandez <luis.hernandez@mocion.mx>
2025-10-31 22:36:31 -0600

    Nuevo desarrollo photoboot

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["storage.googleapis.com"],
+    unoptimized: true,
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
